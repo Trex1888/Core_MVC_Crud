5613aa4958b3ae99b85f6ac9a988c1bc5788a067
Harpua <harpua123@gmail.com>
2024-06-08 00:29:10 -0500

    Add project files.

---
+++ Crud7/Controllers/ProductsController.cs
@@ -0,0 +1,58 @@
+﻿using Crud7.Models;
+using Crud7.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crud7.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var products = _context.Products.OrderBy(p => p.Name).ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductsViewModel productsView)
+        {
+            if (productsView.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "Image is required.");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productsView);
+            }
+
+            return RedirectToAction("Index", "Products");
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        //public async Task<IActionResult> Index()
+        //{
+        //    IEnumerable<Club> clubs = await _clubRepository.GetAll();
+        //    return View(clubs);
+        //}
+
+    }
+}
---
+++ Crud7/Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Crud7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Crud7.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
