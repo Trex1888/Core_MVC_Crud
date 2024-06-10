a8e3f6bd50260ec37ba4e1d45aeeb5f692807f29
Harpua <harpua123@gmail.com>
2024-06-10 07:04:16 -0500

    Project finished.

--- Crud7/Controllers/ProductsController.cs
+++ Crud7/Controllers/ProductsController.cs
@@ -8,16 +8,22 @@ namespace Crud7.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public ProductsController(ApplicationDbContext context)
+        private readonly IWebHostEnvironment _environment;
+
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment)
         {
             _context = context;
+            _environment = environment;
         }
+
+        [HttpGet]
         public IActionResult Index()
         {
             var products = _context.Products.OrderBy(p => p.Name).ToList();
             return View(products);
         }
 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -36,23 +42,121 @@ namespace Crud7.Controllers
                 return View(productsView);
             }
 
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productsView.ImageFile!.FileName);
+
+            string imageFullPath = _environment.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productsView.ImageFile.CopyTo(stream);
+            }
+
+            var product = new Product
+            {
+                Name = productsView.Name,
+                Brand = productsView.Brand,
+                Category = productsView.Category,
+                Price = productsView.Price,
+                Description = productsView.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now
+            };
+
+            _context.Products.Add(product);
+            _context.SaveChanges();
+
             return RedirectToAction("Index", "Products");
         }
 
-        public IActionResult Edit()
+        [HttpGet]
+        public IActionResult Edit(int id)
         {
-            return View();
+            var product = _context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            var productsView = new ProductsViewModel()
+            {
+                Name = product.Name,
+                Brand = product.Brand,
+                Category = product.Category,
+                Price = product.Price,
+                Description = product.Description
+            };
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+            return View(productsView);
         }
 
-        public IActionResult Delete()
+        [HttpPost]
+        public IActionResult Edit(int id, ProductsViewModel productView)
         {
-            return View();
-        }
-        //public async Task<IActionResult> Index()
-        //{
-        //    IEnumerable<Club> clubs = await _clubRepository.GetAll();
-        //    return View(clubs);
-        //}
+            var product = _context.Products.Find(id);
 
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["ProductId"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileName;
+                ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+                return View(productView);
+            }
+
+            string newFileName = product.ImageFileName;
+            if (productView.ImageFile != null)
+            {
+                newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                newFileName += Path.GetExtension(productView.ImageFile!.FileName);
+
+                string imageFullPath = _environment.WebRootPath + "/products/" + newFileName;
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productView.ImageFile.CopyTo(stream);
+                }
+
+                string oldImageFullPath = _environment.WebRootPath + "/products/" + product.ImageFileName;
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+            product.Name = productView.Name;
+            product.Brand = productView.Brand;
+            product.Category = productView.Category;
+            product.Description = productView.Description;
+            product.Price = productView.Price;
+            product.ImageFileName = newFileName;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Products");
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            string imageFullPath = _environment.WebRootPath + "/products/" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+
+            _context.Products.Remove(product);
+            _context.SaveChanges(true);
+
+            return RedirectToAction("Index", "Products");
+        }
     }
 }
